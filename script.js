ce3faae43ce64db744d2f1d92287efaba2db241f
pylover77 <ulquiorragx12@hotmail.com>
2022-06-02 00:43:37 -0300

    form

---
+++ script.js
@@ -0,0 +1,74 @@
+let Validator = {
+    handleSubmit:(event)=>{
+        event.preventDefault();
+        let send = true;
+        let inputs = form.querySelectorAll('input');
+        Validator.clearErrors();
+
+        for(let i=0; i<inputs.length;i++){
+            let input = inputs[i];
+            let check = Validator.checkInput(input);
+            if (check !== true){
+                send = false;
+                Validator.showError(input,check);
+
+            }
+        }
+
+        if(send){
+            form.submit();
+        }
+    },
+    checkInput:(input) =>{
+        let rules = input.getAttribute('data-rules');
+        if(rules !== null){
+            rules = rules.split('|');
+            for (let k in rules){
+                let rDetails = rules[k].split('=');
+                switch (rDetails[0]){
+                    case 'required':
+                        if(input.value ==''){
+                            return 'Este campo deve ser preenchido.';
+
+                        }
+                    break;
+                    case 'min':
+                        if (input.value.length < rDetails[1]){
+                            return 'Este campo deve ter no minimo '+ rDetails[1] +' Caracteres';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != ''){
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if (!regex.test(input.value.toLowerCase())){
+                                return 'Email invalido';
+                            }
+                        }
+
+                }
+            }
+        }
+        return true;
+    },
+    showError:(input, error)=>{
+        input.style.borderColor = '#FF0000';
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+        input.parentElement.insertBefore(errorElement, input.ElementSibling)
+
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++){
+            inputs[i].style = '';
+        }
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0; i<errorElements.length;i++){
+            errorElements[i].remove();
+        }
+    }
+}
+
+let form = document.querySelector('.validator');
+form.addEventListener('submit', Validator.handleSubmit);
